f0cc45e893fd2c04ac9e2cdacd7a0ef43ac86e11
Josh <thequestionking@gmail.com>
2015-11-21 01:50:48 -0800

    added main menu loop function

--- TauNet_class.py
+++ TauNet_class.py
@@ -31,8 +31,7 @@ class TauNetServer(threading.Thread):
 
         server_socket.close()
 
-#A class that acts as a interface to send/recieve messages to/from
-#other users within a TauNet network.
+#A class that acts as a interface to send/recieve messages to/from other users within a TauNet network.
 #It includes an instance of a TauNetServer that is started immmeadiately within a separate thread
 class TauNet():
     def __init__(self):
@@ -40,6 +39,12 @@ class TauNet():
         self.server.setDaemon(True)
         self.server.start()
         self.user_table = {}
+        self.name = ''
+        
+        #the first line of the user_table.txt file corresponds to the owner of the TauNet
+        a_file = open('user_table.txt', 'r')
+        self.name = a_file.readline().split('|')[0]
+        a_file.close()
         
         #read contents in from local file user_table.txt
         a_file = open('user_table.txt', 'r')
@@ -48,6 +53,34 @@ class TauNet():
             self.user_table.update({line[0]:line[1]})
 
         a_file.close()
+
+    #main loop of the program
+    #gives the user options to view/send messages, view user table, and exit
+    def menu(self):
+        choice = 999
+        while(choice != 0):
+            print 'Choose from the following: '
+            print '1 - Send a message to a TauNet user'
+            print '2 - Check for messages recieved from other TauNet users'
+            print '3 - View users in your TauNet network'
+            print '0 - Exit the program'
+            choice = int(raw_input('Enter the # corresponding to your choice: '))
+            if(choice == 1):
+                print 'You chose to send a message to another TauNet user.'
+                self.send_message()
+            else if(choice == 2):
+                print 'You chose to check your messages.'
+            else if(choice == 3):
+                print 'You chose to view users in your network.'
+                self.display_users()
+            else if(choice == 0):
+                print 'You chose to exit. Goodbye.'
+            else:
+                print 'Invalid input. Try again.'
+                
+            raw_input('Press Enter to Continue')
+            clear_screen()
+        
 
     #displays contents of user table stored in self.user_table
     def display_users(self):
@@ -71,7 +104,7 @@ class TauNet():
 
         message = raw_input('Enter a message to send ' + user + ': ')
         
-        ciphertext = encrypt('version: ' + VERSION + '\nfrom: ' self.name + '\nto: ' + user + '\n\n' + message)
+        ciphertext = encrypt('version: ' + VERSION + '\nfrom: ' + self.name + '\nto: ' + user + '\n\n' + message)
 
         #establish a connection and send message to user
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -89,6 +122,6 @@ def clear_screen():
 
 def main():
     a_net = TauNet()
-    a_net.send_message()
+    a_net.menu()
   
 main()
